2a9e22726577890cd43774d194eb79deb1afa46c
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-05-24 11:53:25 +0200

    Fixed hover and onscroll

--- js/header.js
+++ js/header.js
@@ -11,34 +11,3 @@ for (let i = 0; i < navItem.length; i++) {
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('showing');
 });
-
-window.onscroll = function () {
-    if (window.scrollY > 800) {
-        document.querySelector('.nav__item1').style.color = 'var(--black)';
-        document.querySelector('.nav__item2').style.color = 'var(--black)';
-        document.querySelector('.nav__item3').style.color = 'var(--black)';
-        document.querySelector('.nav__item4').style.color = 'var(--black)';
-        hamburger.style.color = 'var(--black)';
-
-        document.querySelector('.logo__container').innerHTML = `
-        <a href="index.html">
-            <img class="logo" src="img/logo-black.svg" alt="Illustrative logo with chair, lamp and table">
-        </a>
-        `;
-
-    } else {
-        document.querySelector('.nav__item1').style.color = 'var(--white)';
-        document.querySelector('.nav__item2').style.color = 'var(--white)';
-        document.querySelector('.nav__item3').style.color = 'var(--white)';
-        document.querySelector('.nav__item4').style.color = 'var(--white)';
-        hamburger.style.color = 'var(--white)';
-
-        document.querySelector('.logo__container').innerHTML = `
-        <a href="index.html">
-            <img class="logo" src="img/logo-white.svg" alt="Illustrative logo with chair, lamp and table">
-        </a>
-        `;
-    };
-};
-
-// cam at least it runs :) ^
---
+++ js/onscroll.js
@@ -0,0 +1,30 @@
+window.onscroll = function () {
+    if (window.scrollY > 800) {
+        document.querySelector('.nav__item1').style.color = 'var(--black)';
+        document.querySelector('.nav__item2').style.color = 'var(--black)';
+        document.querySelector('.nav__item3').style.color = 'var(--black)';
+        document.querySelector('.nav__item4').style.color = 'var(--black)';
+        hamburger.style.color = 'var(--black)';
+
+        document.querySelector('.logo__container').innerHTML = `
+        <a href="index.html">
+            <img class="logo" src="img/logo-black.svg" alt="Illustrative logo with chair, lamp and table">
+        </a>
+        `;
+
+    } else {
+        document.querySelector('.nav__item1').style.color = 'var(--white)';
+        document.querySelector('.nav__item2').style.color = 'var(--white)';
+        document.querySelector('.nav__item3').style.color = 'var(--white)';
+        document.querySelector('.nav__item4').style.color = 'var(--white)';
+        hamburger.style.color = 'var(--white)';
+
+        document.querySelector('.logo__container').innerHTML = `
+        <a href="index.html">
+            <img class="logo" src="img/logo-white.svg" alt="Illustrative logo with chair, lamp and table">
+        </a>
+        `;
+    };
+};
+
+// cam at least it runs :) ^
