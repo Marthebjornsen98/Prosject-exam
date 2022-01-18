4ad303cf55ea670c9b9cb4a0c6795571c5df8fe4
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2022-01-18 23:08:57 +0100

    made it work again

--- js/posts.js
+++ js/posts.js
@@ -1,54 +1,33 @@
-const postsAPI = 'https://noroffcors.herokuapp.com/https://api.bjornsendesign.tech/wp-json/wp/v2/posts';
+// const posts = [
+//   {
+//     img: "img/light-and-woody.jpeg",
+//     title: "Light and woody",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium impedit iusto autem, quo voluptas asperiores nobis eaque voluptatibus recusandae at laboriosam assumenda quos quam explicabo.",
+//   },
+//   {
+//     img: "",
+//     title: "Bedroom wall",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium impedit iusto autem, quo voluptas asperiores nobis eaque voluptatibus recusandae at laboriosam assumenda quos quam explicabo.",
+//   },
+//   {
+//     img: "",
+//     title: "Creme and beige",
+//     description:
+//       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium impedit iusto autem, quo voluptas asperiores nobis eaque voluptatibus recusandae at laboriosam assumenda quos quam explicabo.",
+//   },
+// ];
 
-// API Call
-const getposts = async (url) => {
-    try {
-        document.querySelector('.loading').innerHTML = `
-        <lottie-player src="https://assets4.lottiefiles.com/private_files/lf30_dchle8f3.json"  background="transparent"  speed="1"  style="width: 500px; height: 400px;"  loop  autoplay></lottie-player>
-        `;
+// posts.forEach(({ img, title, description }) => {
+//   document.querySelector(".posts__container").innerHTML += `
+//     <div class="posts__content">
+//         <h2 class="medium__header">${title}</h2>
+//         <div class="next-paragraph"></div>
+//         <p class="body-text">${description}</p>
+//     </div>
+//     <div class="posts__img--container posts-img"></div>
+//     `;
 
-        const response = await fetch(url);
-        const posts = await response.json();
-        console.log(posts);
-
-        posts.forEach(element => {
-            document.querySelector('.posts__container').innerHTML += `
-                <div class="posts__content">
-                    <a href="posts-specific.html?id=${element.id}">
-                        <h2 class="medium__header">${element.title.rendered}</h2>
-                    <a>
-                    <div class="next-paragraph"></div>
-                    <p class="body-text">${element.excerpt.rendered}</p>
-                </div>
-                <a href="posts-specific.html?id=${element.id}">
-                    <div class="posts__img--container posts-img${element.id}"></div>
-                </a>
-            `;
-
-            document.querySelector(`.posts-img${element.id}`).style.backgroundImage = `url(${element.better_featured_image.media_details.sizes.large.source_url})`;
-        });
-
-    } catch (error) {
-        document.querySelector('.alert').innerHTML = showAlert(
-            'An error occured, please contact The Modern Apartment',
-            'danger'
-        );
-        console.log(error);
-
-    } finally {
-        setTimeout(function () {
-            document.querySelector('.alert').innerHTML = ``;
-        }, 3000);
-
-        document.querySelector('.loading').innerHTML = ``;
-    };
-};
-
-getposts(postsAPI);
-
-const seeMoreBtn = document.querySelector('.see-more__btn');
-
-seeMoreBtn.onclick = function () {
-    getposts(postsAPI + `?page=2`);
-    seeMoreBtn.style.display = 'none';
-};
+//   document.querySelector(`.posts-img`).style.backgroundImage = `url(${img})`;
+// });
--- js/script.js
+++ js/script.js
@@ -1,75 +1,57 @@
-const sliderAPI = 'https://noroffcors.herokuapp.com/https://api.bjornsendesign.tech/wp-json/wp/v2/posts';
-const alert = document.querySelector('.alert');
-const linkHeadingElm = document.querySelector('.home-post__text');
+const linkHeadingElm = document.querySelector(".home-post__text");
 
-// API Call
-const sliderImg = async (url) => {
-    try {
-        const response = await fetch(url);
-        const sliderImgResults = await response.json();
-        console.log(sliderImgResults)
+const sliderArray = [
+  {
+    img: "../../img/chair.jpeg",
+    title: "All About The Chair",
+  },
+  {
+    img: "../../img/pond-mirror.jpeg",
+    title: "Pond Mirrors",
+  },
+  {
+    img: "../../img/interior_light.jpeg",
+    title: "Creme and beige",
+  },
+];
 
-        // Slider img
-        sliderImgResults.forEach(element => {
-            if (element.better_featured_image.media_details.sizes.woocommerce_thumbnail.source_url) {
-                document.querySelector('#slider').innerHTML += `
-                    <div class="slider__content">
-                        <a  href="posts-specific.html?id=${element.id}">
-                            <img class="slider__img" src="${element.better_featured_image.media_details.sizes.woocommerce_thumbnail.source_url}">
-                            <h3 class="small__header">${element.title.rendered}</h3>
-                        </a>
-                    </div>
-                `;
-            } else {
-                document.querySelector('#slider').innerHTML += `
-                  <h2>${element.title.rendered}</h2>
-                `;
-            }
-        });
-        slider();
-
-    } catch (error) {
-        alert.innerHTML = showAlert(
-            'An error occured, please contact The Modern Apartment',
-            'danger'
-        );
-
-        console.log(error);
-
-    } finally {
-        setTimeout(function () {
-            alert.innerHTML = '';
-        }, 3000);
-    }
-};
-
-sliderImg(sliderAPI)
+// Slider img
+sliderArray.forEach((element) => {
+  document.querySelector("#slider").innerHTML += `
+            <div class="slider__content">
+                <img class="slider__img" src="${element.img}">
+                <h3 class="small__header">${element.title}</h3>
+                </a>
+            </div>
+        `;
+});
+slider();
 
 // Slider function
 function slider() {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: true,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    arrows: false,
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    arrows: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    });
-};
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+}
